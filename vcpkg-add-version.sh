6783b72fb2e5d2ed73d3db123fc59437c28fade5
JLanda91 <jasperlanda91@gmail.com>
2025-10-14 15:24:35 +0200

    correct vcpkg add version command

--- vcpkg-add-version.sh
+++ vcpkg-add-version.sh
@@ -3,14 +3,6 @@
 set -euo pipefail
 
 # Register vcpkg version
-vcpkg x-add-version sparkplug --overwrite-version
-
-# Copy back resulting sparkplug json
-cp /opt/vcpkg/versions/s-/sparkplug.json ./versions/s-/sparkplug.json
-
-# Copy back sparkplug element of baseline in custom json
-jq '{
-  "default": {
-    "sparkplug": .default.sparkplug
-  }
-}' /opt/vcpkg/versions/baseline.json > ./versions/baseline.json
+vcpkg x-add-version sparkplug \
+  --x-builtin-ports-root=/sparkplug-vcpkg/ports \
+  --x-builtin-registry-versions-dir=/sparkplug-vcpkg/versions
