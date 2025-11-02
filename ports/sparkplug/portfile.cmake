9a7638a2f035932e1df392d0e1f10f8745247c4d
Jasper Landa <jasperlanda91@gmail.com>
2025-11-02 14:03:11 +0100

    Update portfile.cmake

--- ports/sparkplug/portfile.cmake
+++ ports/sparkplug/portfile.cmake
@@ -5,6 +5,8 @@ vcpkg_from_github(
     SHA512 dee624c0f496136a4aec2c1df4ab83c321ceac1fa556480a166d33b84e098416d0bc6041f0d73a095e618c68f7786367273a116d0465dca2bfc08c496571645a
 )
 
+include(vcpkg-cmake)
+
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
 )
